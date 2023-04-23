3795d78e1c28e94892b0cadaecf51fd229c1b7be
Raven <marhforimane@gmail.com>
2023-04-23 13:07:02 -0700

    mainh

--- main.h
+++ main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdef.h>
-#include <string.h>
 #include <stdarg.h>
-#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
@@ -21,18 +18,10 @@
 #define S_SHORT 1
 
 /**
- * typedef struct fmt fmt_t - format structure
- *
- * @fmt:  string representing a format
- * @fm_t: function pointer
- */
-typedef struct fmt fmt_t;
-
-/**
  * struct fmt - Struct op
  *
- * @fmt: the format
- * @f: the function
+ * @fmt: format
+ * @f: function
  */
 struct fmt
 {
@@ -40,19 +29,26 @@ struct fmt
 	int (*f)(va_list, char[], int, int, int, int);
 };
 
-int _putchar(char c);
+
+/**
+ * typedef struct fmt fmt_t - format structure
+ *
+ * @fmt: format
+ * @fm_t: function
+ */
+typedef struct fmt fmt_t;
+
 int _printf(const char *format, ...);
-int _funcopr(const char *format, prf opr[], va_list list);
 void print_buffer(char buffer[], int *buff_ind);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
-
-int print_c(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
-int print_s(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
-int print_mod(va_list types, char buffer[],
-		int flags, int width, int precision, int size);
-
+int print_char(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags,
+		int width, int precision, int size);
 
 #endif
