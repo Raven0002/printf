192a204aa7d6bac58d18792991872a73e61ffc82
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-23 07:15:05 -0700

    printf

--- printf.c
+++ printf.c
@@ -24,3 +24,54 @@ int _printf(const char *format, ...)
 		va_start(list, format);
 		va_end(list);
 	}
+}
+
+/**
+ * _funcion - Helper function to print and call functions.
+ * @format: String recieved.
+ * @opr: special options.
+ * @list: arguments
+ * Return: number of chars printed
+ */
+
+int _funcion(const char *format, prf opr[], va_list list)
+{
+	int count = 0, x, y;
+
+	for (x = 0; format[x] != '\0'; x++)
+	{
+		if (format[x] == '%')
+		{
+			if (format[x + 1] == '\0')
+			{
+				return (-1);
+			}
+			for (y = 0; ops[y].op != NULL; y++)
+			{
+				if (format[x + 1] == ops[y].op[0])
+				{
+					count = count + ops[y].f(list);
+					break;
+				}
+			}
+			if (opr[y].op == NULL && format[x + 1] != ' ')
+			{
+				if (format[x + 1] != '\0')
+				{
+					_putchar(format[x]);
+					_putchar(format[x + 1]);
+					count = count + 2;
+				}
+				else
+					return (-1);
+			}
+			x = x + 1;
+		}
+		else
+		{
+			_putchar(format[x]);
+			count = count + 1;
+		}
+	}
+	return (count);
+}
