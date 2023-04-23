87bd357f4f6f3c3acb8e773503cebf5de7b58d27
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-04-23 09:02:06 -0700

    Prototype file

--- main.h
+++ main.h
@@ -6,8 +6,23 @@
 #include <stdef.h>
 #include <string.h>
 
+/**
+ * struct pr - struct for operations
+ * @op: operation
+ * @f: to be listed
+ * Description: Checks if there's an operation
+ */
+typedef struct pr
+{
+	char *op;
+	int (*f)(va_list);
+} prf;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _funcopr(const char *format, prf opr[], va_list list);
+int print_c(va_list list);
+int print_s(va_list list);
+int print_mod(va_list list);
 
 #endif
