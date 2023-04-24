db8a5ca6d2b9604ca3bfb2ec5bc7e3c56df443e0
Raven <marhforimane@gmail.com>
2023-04-23 17:37:14 -0700

    printf

--- main.h
+++ main.h
@@ -50,5 +50,24 @@ int print_string(va_list types, char buffer[], int flags,
 		int width, int precision, int size);
 int print_percent(va_list types, char buffer[], int flags,
 		int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
 #endif
